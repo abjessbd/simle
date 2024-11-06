7a8cc19d7093942a9dcfe8359ec75a6dcb6b920a
abjessbd <abjessbd@gmail.com>
2024-11-06 13:00:10 +0600

    Callback Function modified

--- callback.js
+++ callback.js
@@ -1,9 +1,15 @@
-const callback = (cb) =>{
+const callback = (cb = val => val ** 2) =>{
     const a = 10;
     const b = 12;
     const total = a + b;
     return cb(total);
 }
+// const callback = (cb) =>{
+//     const a = 10;
+//     const b = 12;
+//     const total = a + b;
+//     return cb(total);
+// }
 
 // const squire = (value) =>{
 //     return value ** 2;
@@ -11,5 +17,5 @@ const callback = (cb) =>{
 
 // const grandTotal = callback(squire);
 
-// console.log(callback());
-console.log(callback(val => val ** 2));
+console.log(callback());
+// console.log(callback(val => val ** 2));
